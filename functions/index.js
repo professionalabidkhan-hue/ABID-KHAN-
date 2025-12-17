9cdbacd38488076b9500ac60b79408bf06195a12
MUHAMMAD ABID RAFIQ KHAN <professionalabidkhan@gmail.com>
2025-12-17 21:04:15 +0500

    Update index.js

--- functions/index.js
+++ functions/index.js
@@ -1,6 +1,12 @@
 const functions = require("firebase-functions");
+const express = require("express");
+const app = express();
+app.use(express.json());
 
-exports.chat = functions.https.onRequest((req, res) => {
-  const { question } = req.body;
-  res.json({ answer: `You asked: ${question}. Reply from Firebase Cloud API.` });
+app.post("/chat", (req, res) => {
+  const userMessage = req.body.message;
+  // For now, just echo back
+  res.json({ reply: "You said: " + userMessage });
 });
+
+exports.chat = functions.https.onRequest(app);
